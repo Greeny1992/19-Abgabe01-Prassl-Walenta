d683900d3341f7c71c7a692b574b16b6e288bf69
Greeny1992 <georg.prassl@edu.campus02.at>
2019-03-27 15:15:39 +0100

    Tast 2 for student 1. Implement getMin() and test it in main.
    I implemented the getMin() method. It has a simple search algorithm.
    After that i tested it in the main class with random numbers.

--- GitCalculatorExercise/src/at/fhj/iit/Main.java
+++ GitCalculatorExercise/src/at/fhj/iit/Main.java
@@ -7,9 +7,20 @@ public class Main {
 
 	public static void main(String[] args) {
 		List<Integer> intList = new ArrayList<>();
+		addValue(2,intList);
+		addValue(5,intList);
+		addValue(4,intList);
+		addValue(99,intList);
+		addValue(-1,intList);
+		System.out.println("The smallest number is: " + getMinimum(intList));
 	}
 	public static int getMinimum(List<Integer> intList){
-		return 0;
+		int min = intList.get(0);
+		for(int actualVal : intList){
+			if(actualVal < min)
+				min = actualVal;
+		}
+		return min;
 	}
 	public static int getMaximum(List<Integer> intList){
 		return 0;
