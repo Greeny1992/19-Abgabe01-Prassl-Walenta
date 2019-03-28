dcf08d63c69da12678df32353741f0c54a33d52d
Greeny1992 <georg.prassl@edu.campus02.at>
2019-03-28 15:25:50 +0100

    Final commit
    Deleted added testvalues.
    Finalized project.

--- GitCalculatorExercise/src/at/fhj/iit/Main.java
+++ GitCalculatorExercise/src/at/fhj/iit/Main.java
@@ -7,8 +7,6 @@ public class Main {
 		calc.addValue(2);
 		calc.addValue(5);
 		calc.addValue(4);
-		calc.addValue(99);
-		calc.addValue(-1);
 		System.out.println("The smallest number is: " + calc.getMinimum());
 		System.out.println("The largest number is: " + calc.getMaximum());
 		System.out.println("The sum of all numbers is: " + calc.sum());
